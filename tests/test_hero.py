0150d3732132d4af7a3de5a33fc792a3fe27a17e
Ignacio de Miguel Diaz <imigueldiaz@gmail.com>
2024-11-01 08:39:51 +0100

    [testing] Hero test lightly improved

--- tests/test_hero.py
+++ tests/test_hero.py
@@ -8,7 +8,7 @@ from src.heromonsters import SPRITES_PATH
 class TestHeroInit(unittest.TestCase):
 
     @patch('pygame.image.load', return_value=MagicMock())
-    def test_init_valid_args(self, mock_image_load):
+    def test_init_valid_args(self, _):
         """Tests that Hero's __init__ method correctly assigns all the passed arguments
         to the corresponding instance variables when given valid arguments."""
         image_path = 'path/to/image.png'
@@ -30,7 +30,7 @@ class TestHeroInit(unittest.TestCase):
         self.assertEqual(hero.last_collision_time, 0)
 
     @patch('pygame.image.load', side_effect=pygame.error)
-    def test_init_invalid_image_path(self, mock_image_load):
+    def test_init_invalid_image_path(self, _):
         """Tests that Hero's __init__ method raises a pygame.error when given an image path
         that cannot be loaded."""
         image_path = 'invalid/path'
@@ -43,7 +43,7 @@ class TestHeroInit(unittest.TestCase):
             Hero(image_path, x, y, hero_speed, window_width, *groups)
 
     @patch('pygame.image.load', side_effect=pygame.error)
-    def test_invalid__empty_image_path(self, mock_image_load):
+    def test_invalid__empty_image_path(self, _):
         """Tests that Hero's __init__ method raises a ValueError when given an image path
         that is an empty string."""
         image_path = ''
@@ -66,6 +66,7 @@ class TestHeroInit(unittest.TestCase):
         groups = [MagicMock()]
         hero =Hero(image_path, x, y, hero_speed, window_width, *groups)
         self.assertEqual(hero.image_path, image_path)
+        self.assertIsNotNone(hero.image)
         self.assertEqual(hero.x, x)
         self.assertEqual(hero.y, y)
         self.assertEqual(hero.speed, hero_speed)
@@ -77,7 +78,7 @@ class TestHeroInit(unittest.TestCase):
         self.assertEqual(hero.last_collision_time, 0)
 
     @patch('pygame.image.load', return_value=MagicMock())
-    def test_init_invalid_x(self, mock_image_load):
+    def test_init_invalid_x(self, _):
         """Tests that Hero's __init__ method raises a ValueError when given an x-coordinate
         that is negative."""
         image_path = 'path/to/image.png'
@@ -90,7 +91,7 @@ class TestHeroInit(unittest.TestCase):
             Hero(image_path, x, y, hero_speed, window_width, *groups)
 
     @patch('pygame.image.load', return_value=MagicMock())
-    def test_init_invalid_y(self, mock_image_load):
+    def test_init_invalid_y(self, _):
         """Tests that Hero's __init__ method raises a ValueError when given a y-coordinate
         that is negative."""
         image_path = 'path/to/image.png'
@@ -103,7 +104,7 @@ class TestHeroInit(unittest.TestCase):
             Hero(image_path, x, y, hero_speed, window_width, *groups)
 
     @patch('pygame.image.load', return_value=MagicMock())
-    def test_init_invalid_hero_speed(self, mock_image_load):
+    def test_init_invalid_hero_speed(self, _):
         """Tests that Hero's __init__ method raises a ValueError when given a hero speed
         that is negative."""
         image_path = 'path/to/image.png'
@@ -116,7 +117,7 @@ class TestHeroInit(unittest.TestCase):
             Hero(image_path, x, y, hero_speed, window_width, *groups)
 
     @patch('pygame.image.load', return_value=MagicMock())
-    def test_init_invalid_window_width(self, mock_image_load):
+    def test_init_invalid_window_width(self, _):
         """Tests that Hero's __init__ method raises a ValueError when given a window width
         that is negative."""
         image_path = 'path/to/image.png'
